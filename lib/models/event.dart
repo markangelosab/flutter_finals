21b4472905a00234f3d1cfe23ee6a55488263506
markangelosab <markangelosab@gmail.com>
2024-12-06 10:08:33 +0800

    flutter

---
+++ lib/models/event.dart
@@ -0,0 +1,6 @@
+class Event {
+  String title;
+  String date;
+
+  Event({required this.title, required this.date});
+}
---
+++ lib/pages/events_page.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import '../widgets/event_card.dart';
+
+class EventsPage extends StatefulWidget {
+  @override
+  _EventsPageState createState() => _EventsPageState();
+}
+
+class _EventsPageState extends State<EventsPage> {
+  List<Map<String, String>> events = [
+    {"title": "Meeting with Team", "date": "2024-12-10"},
+    {"title": "Client Presentation", "date": "2024-12-12"},
+    {"title": "Company Party", "date": "2024-12-15"},
+  ];
+
+  void _editEvent(int index) {
+    final event = events[index];
+    final titleController = TextEditingController(text: event['title']);
+    final dateController = TextEditingController(text: event['date']);
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Edit Event'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(labelText: 'Event Title'),
+            ),
+            TextField(
+              controller: dateController,
+              decoration: InputDecoration(labelText: 'Event Date'),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              setState(() {
+                events[index] = {
+                  'title': titleController.text,
+                  'date': dateController.text,
+                };
+              });
+              Navigator.of(context).pop();
+            },
+            child: Text('Save'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Cancel'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _deleteEvent(int index) {
+    setState(() {
+      events.removeAt(index);
+    });
+  }
+
+  void _addEvent() {
+    final titleController = TextEditingController();
+    final dateController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Add New Event'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(labelText: 'Event Title'),
+            ),
+            TextField(
+              controller: dateController,
+              decoration: InputDecoration(labelText: 'Event Date'),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              setState(() {
+                events.add({
+                  'title': titleController.text,
+                  'date': dateController.text,
+                });
+              });
+              Navigator.of(context).pop();
+            },
+            child: Text('Save'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Cancel'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Events')),
+      body: ListView.builder(
+        itemCount: events.length,
+        itemBuilder: (context, index) {
+          return EventCard(
+            title: events[index]['title']!,
+            date: events[index]['date']!,
+            onEdit: () => _editEvent(index),
+            onDelete: () => _deleteEvent(index),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addEvent,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
